53f2317d3985a8b3aa7bc236b3d105a0bf5f84d9
yanna-torres <torres.yanna03@gmail.com>
2024-02-22 11:04:05 -0300

    verify email and success screen

---
+++ lib/common/widgets/success_screen/success_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/common/styles/spacing_styles.dart';
+import 'package:flutter_ecommerce/utils/constants/sizes.dart';
+import 'package:flutter_ecommerce/utils/constants/text_strings.dart';
+import 'package:flutter_ecommerce/utils/helpers/helper_functions.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+    this.onPressed,
+  });
+
+  final String image, title, subTitle;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: TSpacingStyle.paddingWithAppbarHeight * 2,
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              // Image
+              Image.asset(
+                image,
+                width: THelperFunctions.screenWidth(context) * 0.6,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              // Title & Subtitle
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Text(
+                subTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              // Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text(TTexts.tContinue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/features/authentication/screens/signup/verify_email.dart';
 import 'package:flutter_ecommerce/features/authentication/screens/signup/widgets/terms_and_conditions.dart';
 import 'package:flutter_ecommerce/utils/constants/sizes.dart';
 import 'package:flutter_ecommerce/utils/constants/text_strings.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class TSignupForm extends StatelessWidget {
@@ -83,7 +85,7 @@ class TSignupForm extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: () => Get.to(() => const VerifyEmailScreen()),
               child: const Text(TTexts.createAccount),
             ),
           )
